a843f74dda2f99cc8965d2c260e15c04c588c99b
Denis Paskin <paskin.denis.00@gmail.com>
2023-04-14 17:20:26 +0200

    changed the paths

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Box } from '@mui/material'
-import Navbar from './components/Navbar'
-import Feed from './components/Feed'
-import VideoDetail from './components/VideoDetail'
-import ChannelDetail from './components/ChannelDetail'
-import SearchFeed from './components/SearchFeed'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed
+} from './components';
 
-const  App = () => (
+const App = () => (
   <BrowserRouter>
-   <Box sx={{ backgroundColor: '#000'}}>
-    <Navbar/>
-    <Routes>
-      <Route path="/" exact element={<Feed/>}/>
-      <Route path="/video/:id" exact element={<VideoDetail/>}/>
-      <Route path="/channel/:id" exact element={<ChannelDetail/>}/>
-      <Route path="/search/:searchTerm" exact element={<SearchFeed/>}/>
-    </Routes>
-   </Box>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" exact element={<VideoDetail />} />
+        <Route path="/channel/:id" exact element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+      </Routes>
+    </Box>
   </BrowserRouter>
-)
-  
+);
 
-
-export default App
+export default App;
